67f5835b4b69df836d9c9930adadb891671e5eea
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-10-13 22:29:29 -0700

    done

--- exercice.py
+++ exercice.py
@@ -4,15 +4,28 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	num_letters = 0
+	for chr in text:
+		num_letters = num_letters + int(chr.isalnum())
+	return num_letters
 
 def get_word_length_histogram(text):
-	return [0]
+	histogram = [0]
+	for word in text.split():
+		length = get_num_letters(word)
+		if length >= len(histogram):
+			histogram += [0] * (length - len(histogram) + 1)
+		histogram[length] += int(length != 0)
+
+	return histogram
 
 def format_histogram(histogram):
-	ROW_CHAR = "*"
-
-	return ""
+	result = ""
+	for i, elem in enumerate(histogram):
+		if i == 0:
+			continue
+		result += f"{i : >{alignment}} {ROW_CHAR * elem}" + "\n"
+	return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
